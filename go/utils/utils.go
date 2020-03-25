a5f1cf6ddecb51a9bf50d8ec2a9c4a7b73460064
David Cunningham <dcunningham@datafactory.us>
2020-03-25 14:26:48 -0400

    added ArrayContains method

--- go/utils/utils.go
+++ go/utils/utils.go
@@ -1,3 +1,12 @@
 // Package utils provides utilities to handle common activities such as logging, error handling, etc.
 //
 package utils
+
+func ArrayContains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
