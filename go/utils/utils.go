6b4e25711f32c240752acf894ba9f95a5028f7ad
David Cunningham <david.cunningham@insipher.com>
2020-07-02 14:58:37 -0400

    added IsEditor check

--- go/utils/utils.go
+++ go/utils/utils.go
@@ -2,6 +2,8 @@
 //
 package utils
 
+import "encoding/json"
+
 func ArrayContains(arr []string, str string) bool {
 	for _, a := range arr {
 		if a == str {
@@ -22,4 +24,19 @@ func IndexOf(element string, arr []string) int {
 
 func RemoveIndex(arr []string, index int) []string {
 	return append(arr[:index], arr[index+1:]...)
+}
+
+func IsEditor(userID string, editors json.RawMessage) (bool, error) {
+	hasAccess := false
+	var editorIDs []string
+	err := json.Unmarshal(editors, &editorIDs)
+	if err != nil {
+		return false, err
+	}
+
+	// check to see if user already is in favorties list, if so just return
+	if IndexOf(userID, editorIDs) > 0 {
+		return true, nil
+	}
+	return hasAccess, nil
 }
